8e226af0d7f14bdc0595c50866d318bc316d6fa7
John Enahoro <enahoro.john.a@gmail.com>
2022-06-30 13:25:17 -0700

    c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+nclude <stdio.h>
+/**
+ *
+ *  * main - code to put a message
+ *
+ *   * Return: Always 0
+ *
+ *    */
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
