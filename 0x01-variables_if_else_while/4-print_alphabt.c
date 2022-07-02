c029ea923a9fca171fab0655edd58961cda68646
John Enahoro <enahoro.john.a@gmail.com>
2022-07-01 22:25:20 -0700

    upd

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,9 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-		char low, e, q;
-			e = 'e';
-				q = 'q';
-					for (low = 'a'; low <= 'z'; low++)
-							{
-										if (low != e && low != q)
-														putchar(low);
-											}
-						putchar('\n');
-							return (0);
+int main(void){char low, e, q;e = 'e';q = 'q';
+	for (low = 'a'; low <= 'z'; low++){
+           if (low != e && low != q) putchar(low);}
+		putchar('\n');
+ return (0);
 }
